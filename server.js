33a69b441d18de4b280e9f86c47cdc0a27af6ae6
FanZhiheng <fanzhihe@msu.edu>
2019-03-22 22:58:37 -0400

    first try

---
+++ server.js
@@ -0,0 +1,53 @@
+var todos = require('./todos.js')
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 8080
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.get('/', function (request, response) {
+  response.json({
+    welcome: 'welcome to FanZhiheng\'s API!'
+  })
+})
+
+app.get('/todos', function (request, response) {
+  response.json(todos)
+})
+
+app.get('/todos/:id', function (request, response) {
+  if ((todos[request.params.id])) {
+    response.json(todos[request.params.id])
+  } else {
+    response.status(404).end('sorry, no such product: ' + request.params.id)
+  }
+})
+
+app.post('/todos', function (request, response) {
+  var slug = request.body.what.trim().toLowerCase().split(' ').join('-')
+  todos[slug] = {
+    what: request.body.what,
+    when: request.body.when
+  }
+  response.redirect('/todos/' + slug)
+})
+
+app.delete('/todos/:id', function (request, response) {
+  delete todos[request.params.id]
+  response.redirect('/todos')
+})
+
+app.put('/todos/:id', function (request, response) {
+  var todo = todos[request.params.id]
+  if (request.body.what !== undefined) {
+    todo.what = request.body.what.trim()
+  }
+  if (request.body.when !== undefined) {
+    todo.when = request.body.when.trim()
+  }
+  response.redirect('/todos/' + request.params.id)
+})
+app.use(function (request, response, next) {
+  response.status(404).end(request.url + ' not found')
+})
+app.listen(port)
---
+++ todos.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'eat-lunch': {
+    what: 'eat lunch',
+    when: '11:00'
+  },
+  'play-games': {
+    what: 'play games',
+    when: '14:00'
+  },
+  'write-MI449': {
+    what: 'write MI449',
+    when: '16:00'
+  }
+}
